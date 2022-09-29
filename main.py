85d81189ece2c79d89ce9427143e66a393c90324
Wyatt School <wwebb@lakelandcollege.edu>
2022-09-28 21:36:12 -0500

    Added form and supporting code to take input.

--- main.py
+++ main.py
@@ -3,8 +3,46 @@ from caesar import rotate_string
 
 app = Flask('app')
 
+form = """<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+        <form action="/" method="POST">
+            <label for="rot"><label>
+						Rotate by:
+						<input type="text" name="rot" value="0"></input>
+        <textarea name="text" placeholder="Enter some text to encrypt here.">{0}</textarea>
+        <button type="submit">Submit Query</button>
+        </form>
+    </body>
+</html>"""
+
 @app.route('/')
 def index():
-  return 'Hello'
+  return form.format("")
+
+@app.route('/', methods=['POST'])
+def encrypt():
+	rotate = int(request.form['rot'])
+	form_text = request.form['text']
+	new_text = rotate_string(form_text,rotate)
+	return form.format(new_text)
 
 app.run(host='0.0.0.0', port=8080)
